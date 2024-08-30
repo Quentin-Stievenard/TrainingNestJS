3eae77af9be477c1353b02597455f0f9d0dd5217
QStieven <qstievenard@gmail.com>
2024-08-31 00:56:39 +0200

    Fix(src/user): Finish User controller

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,55 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  NotFoundException,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { UserService } from './user.service';
+import { RoleService } from 'src/role/role.service';
+import { User } from './user.entity';
+import { CreateUserDto, UpdateUserDto } from './dto';
 
-@Controller()
-export class UserController {}
+@Controller('users')
+export class UserController {
+  constructor(
+    private userService: UserService,
+    private roleService: RoleService,
+  ) {}
+
+  @Get()
+  getAllUser(): Promise<User[]> {
+    return this.userService.getAllUser();
+  }
+
+  @Get(':id')
+  getUserById(@Param('id') id: number): Promise<User> {
+    return this.userService.getUserById(id);
+  }
+
+  @Post()
+  createUser(@Body() createUserDto: CreateUserDto): Promise<User> {
+    return this.userService.createUser(createUserDto);
+  }
+
+  @Put(':id')
+  updateUser(
+    @Param('id') id: number,
+    @Body() updateUserDto: UpdateUserDto,
+  ): Promise<User> {
+    return this.userService.updateUser(id, updateUserDto);
+  }
+
+  @Delete(':id')
+  removeUser(@Param('id') id: number): string {
+    const success = this.userService.removeUser(id);
+    if (!success) {
+      throw new NotFoundException('User not found');
+    }
+
+    return 'User deleted successfully';
+  }
+}
