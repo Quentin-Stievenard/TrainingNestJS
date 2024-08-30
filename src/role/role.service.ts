dcf5c6e6623b9c70bae10c4930a44f0225790f3b
QStieven <qstievenard@gmail.com>
2024-08-31 01:12:14 +0200

    Fix(src/role): Write Role service

--- src/role/role.service.ts
+++ src/role/role.service.ts
@@ -1,4 +1,33 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Role } from './role.entity';
+import { Repository } from 'typeorm';
+import { CreateRoleDto } from './dto/createRole.dto';
 
 @Injectable()
-export class RoleService {}
+export class RoleService {
+  constructor(
+    @InjectRepository(Role)
+    private roleRepository: Repository<Role>,
+  ) {}
+
+  getAllRoles(): Promise<Role[]> {
+    return this.roleRepository.find();
+  }
+
+  getRoleById(id: number): Promise<Role> {
+    return this.roleRepository.findOne({ where: { id } });
+  }
+
+  async createRole(createRoleDto: CreateRoleDto): Promise<Role> {
+    const role = await this.roleRepository.create(createRoleDto);
+
+    return this.roleRepository.save(role);
+  }
+
+  async removeRole(id: number): Promise<void> {
+    const role = await this.getRoleById(id);
+
+    await this.roleRepository.remove(role);
+  }
+}
